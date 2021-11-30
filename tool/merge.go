efb5a6d90bc0e93f4c72906cef26eca4c6b128c4
takoyaki-3 <mail@takoyaki3.com>
2021-11-30 09:43:21 +0900

    merge min maxを分離

---
+++ tool/merge.go
@@ -0,0 +1,60 @@
+package tool
+
+import (
+	minmax "github.com/takoyaki-3/minmaxRouting"
+)
+
+type Id2Ids struct {
+	Nodeid2nodeids map[minmax.NodeIdType]minmax.NodeIdType
+	Edgeid2edgeids map[minmax.EdgeIdType]minmax.EdgeIdType
+	Nodeid2componentids map[minmax.NodeIdType]int
+	Edgeid2componentids map[minmax.EdgeIdType]int
+
+	OldNodeid2nodeids []map[minmax.NodeIdType]minmax.NodeIdType
+	OldEdgeid2edgeids []map[minmax.EdgeIdType]minmax.EdgeIdType
+}
+
+func MergeMinMaxGraphs(gs []*minmax.Graph)(mg *minmax.Graph,id2ids *Id2Ids){
+	var numNodes,numEdges int
+
+	mg = &minmax.Graph{}
+
+	id2ids = &Id2Ids{
+		Nodeid2nodeids: map[minmax.NodeIdType]minmax.NodeIdType{},
+		Edgeid2edgeids: map[minmax.EdgeIdType]minmax.EdgeIdType{},
+		Nodeid2componentids: map[minmax.NodeIdType]int{},
+		Edgeid2componentids: map[minmax.EdgeIdType]int{},
+		OldNodeid2nodeids: make([]map[minmax.NodeIdType]minmax.NodeIdType, len(gs)),
+		OldEdgeid2edgeids: make([]map[minmax.EdgeIdType]minmax.EdgeIdType, len(gs)),
+	}
+
+	for j,g:=range gs{
+		Nodeid2nodeids := map[minmax.NodeIdType]minmax.NodeIdType{}
+		Edgeid2edgeids := map[minmax.EdgeIdType]minmax.EdgeIdType{}
+		id2ids.OldNodeid2nodeids[j] = map[minmax.NodeIdType]minmax.NodeIdType{}
+		id2ids.OldEdgeid2edgeids[j] = map[minmax.EdgeIdType]minmax.EdgeIdType{}
+
+		for i,_:=range g.Nodes{
+			Nodeid2nodeids[minmax.NodeIdType(i)] = minmax.NodeIdType(numNodes+i)
+			id2ids.Nodeid2nodeids[minmax.NodeIdType(numNodes+i)] = minmax.NodeIdType(i)
+			id2ids.Nodeid2componentids[minmax.NodeIdType(numNodes+1)] = j
+			id2ids.OldNodeid2nodeids[j][minmax.NodeIdType(i)] = minmax.NodeIdType(numNodes+i)
+		}
+		for i,e:=range g.Edges{
+			Edgeid2edgeids[minmax.EdgeIdType(i)] = minmax.EdgeIdType(numEdges+i)
+			id2ids.Edgeid2edgeids[minmax.EdgeIdType(numEdges+i)] = minmax.EdgeIdType(i)
+			id2ids.Edgeid2componentids[minmax.EdgeIdType(numEdges+i)] = j
+			id2ids.OldEdgeid2edgeids[j][minmax.EdgeIdType(i)] = minmax.EdgeIdType(numNodes+i)
+			mg.Edges = append(mg.Edges, minmax.Edge{
+				FromId: Nodeid2nodeids[e.FromId],
+				ToId: Nodeid2nodeids[e.ToId],
+				Weight: e.Weight,
+				ViaNodes: e.ViaNodes,
+			})
+		}
+		numNodes += len(g.Nodes)
+		numEdges += len(g.Edges)
+	}
+	mg.Nodes = make([]minmax.Node, numNodes)
+	return mg,id2ids
+}
